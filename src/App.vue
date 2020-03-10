2f8e193be3f91fe63c350353ea2c86db8b7b0634
fujicho <nnnn.sohei.fujita@gmail.com>
2020-03-10 19:16:14 +0900

    active-class,exactの記述

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
   <div style="width: 700px;margin: auto; padding-top:50px">
     <nav>
-      <router-link to="/" class="link">Home</router-link>
-      <router-link to="/users" class="link">users</router-link>
+      <router-link to="/" active-class="link--active" class="link" exact>Home</router-link>
+      <router-link to="/users"
+      active-class="link--active" class="link" exact>users</router-link>
     </nav>
     <router-view></router-view>
   </div>
 </template>
 
 <style>
-  .link{
-    margin-right: 10px;
-  }
+.link {
+  margin-right: 10px;
+}
+.link--active{
+  font-size: 20px;
+}
 </style>
