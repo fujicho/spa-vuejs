2a213c2b01d19820d1cba40d555517cd686a57e6
fujicho <nnnn.sohei.fujita@gmail.com>
2020-03-11 23:20:25 +0900

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div style="width: 700px;margin: auto; padding-top:50px">
     <router-view name="header"></router-view>
-    <router-view></router-view>
+    <transition name="fade" mode="out-in">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
