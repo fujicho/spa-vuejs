a4feb08c6bef4e1e78f1b405597352248ace0f70
fujicho <nnnn.sohei.fujita@gmail.com>
2020-03-13 17:15:05 +0900

    mapGetters

--- src/views/HeaderHome.vue
+++ src/views/HeaderHome.vue
@@ -4,8 +4,23 @@
     <router-link to="/" active-class="link--active" class="link" exact>Home</router-link>
     <router-link :to="{name: 'users-id-profile',params: {id:1},hash:'#next-user'}"
     active-class="link--active" class="link" exact>users</router-link>
+    <button @click="increment">+1</button>
+    <button @click="decrement">-1</button>
   </nav>
 </template>
+
+<script>
+export default {
+  methods: {
+    increment(){
+      this.$store.state.count++
+    },
+    decrement(){
+      this.$store.state.count--
+    },
+  }
+}
+</script>
 
 <style scoped> 
 .link {
