21456d84ad26f0bdcfa310f52b4bd55a91318875
fujicho <nnnn.sohei.fujita@gmail.com>
2020-03-13 18:44:55 +0900

    モジュール化

--- src/store/index.js
+++ src/store/index.js
@@ -1,38 +1,28 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import count from "./modules/count";
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    count:2,
     message:''
   },
   getters: {
-    doubleCount: state=> state.count *2,
-    tripleCount: state=> state.count *3,
     message: state=> state.message
   },
   mutations:{
-    increment(state,number){
-      state.count += number;
-    },
-    decrement(state,number){
-      state.count -= number;
-    },
     updateMessage(state,newMessage){
       state.message = newMessage;
     }
   },
   actions: {
-    increment(context,number){
-      context.commit('increment',number)
-    },
-    decrement(context,number){
-      context.commit('decrement',number)
-    },
+    
     updateMessage({commit},newMessage){
       commit("updateMessage",newMessage);
     }
+  },
+  modules: {
+    count
   }
 });
